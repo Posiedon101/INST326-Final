9eab00788fa4a88f55fcc7e0b3790ff0b347d658
Jason Carlin <jcarlin1@umd.edu>
2022-11-17 13:21:14 -0500

    Created SKeleton of Project

---
+++ skeleton.py
@@ -0,0 +1,40 @@
+from argparse import ArgumentParser
+import sys
+
+class Person:
+    """_summary_
+    """
+    def __init__(self, name):
+        """_summary_
+
+        Args:
+            name (_type_): _description_
+        """
+        
+    def add_person():
+        """_summary_
+        """
+        
+    def remove_person():
+        """_summary_
+        """
+        
+    def create_appt():
+        """_summary_
+        """
+        
+    def __repr__(self):
+        """_summary_
+        """
+        
+def main():
+    """_summary_
+    """
+    
+def export():
+    """
+    """
+    
+def parse_args():
+    """_summary_
+    """
